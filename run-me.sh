eef0f1d2db18352c292648ca8701ec382570b4be
Chris Hoey <choey@mortardata.com>
2013-07-31 11:18:34 -0400

    Fixing logic to check for box and adding a check to be sure vagrant 1.2.x is installed as the vagrant config format is 1.2.x and wont work with the older vagrant

--- run-me.sh
+++ run-me.sh
@@ -2,9 +2,19 @@
 
 set -e
 
-BOXNAME="centos-6.4-x86-64"
+VAGRANT_VERSION=`vagrant -v | awk '{print $3}' | awk -F '.' '{print $2}'`
+if ! [ $VAGRANT_VERSION -gt 1 ]; then
+    echo ""
+    echo "ERROR: The installed Vagrant version is too low. Please install Vagrant 1.2.0 or higher."
+    echo ""
+    exit 1 
+fi
 
-if vagrant boxes | grep "$BOXNAME"; then
+BOXNAME="centos-6.4-x86-64"
+BOXNAME_EXISTS=`vagrant box list | grep "$BOXNAME" | wc -l`
+
+if [ $BOXNAME_EXISTS -eq 0 ]; then
+    echo "Vagrant does not have the box of $BOXNAME so downloading it nowN"
     vagrant box add "$BOXNAME" "http://developer.nrel.gov/downloads/vagrant-boxes/CentOS-6.4-x86_64-v20130427.box"
 fi
 
